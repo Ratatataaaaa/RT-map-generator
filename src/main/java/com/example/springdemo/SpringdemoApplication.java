7a146d51fda1d631ae43b7f8b4049f10170a8126
Roman Borisov <Rik_sanchez@bk.ru>
2021-01-17 07:51:44 +0300

    TODO: Fix tempResolver

--- src/main/java/com/example/springdemo/SpringdemoApplication.java
+++ src/main/java/com/example/springdemo/SpringdemoApplication.java
@@ -2,11 +2,14 @@ package com.example.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.awt.image.BufferedImage;
+
 
 @SpringBootApplication
 public class SpringdemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringdemoApplication.class, args);
 	}
 
--- src/main/java/com/example/springdemo/controllers/MainController.java
+++ src/main/java/com/example/springdemo/controllers/MainController.java
@@ -1,17 +1,32 @@
 package com.example.springdemo.controllers;
 
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
 	@GetMapping("/")
-	public String hello(Model model) {
+	public String start(@RequestParam(name="Server") String server,Model model) {
 		model.addAttribute("title", "Home page.");
+		model.addAttribute("server", server);
+		return "start";
+	}
+
+	@GetMapping("/hello")
+	public String helloPage() {
 		return "hello";
 	}
 
+	@GetMapping("/goodbye")
+	public String goodByePage() {
+		return "goodbye";
+	}
+
+	@GetMapping("/exit")
+	public String exit() {
+		return "exit";
+	}
 }
